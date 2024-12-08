118d590a2ca34316ce720855d781453321941281
Bobby Lee <bobbylee.dev@gmail.com>
2024-12-08 15:03:56 -0800

    adds zsh to install script

--- install.sh
+++ install.sh
@@ -28,6 +28,10 @@ create_symlink "$DOTFILES_DIR/nvim" "$HOME/.config/nvim"
 # Aerospace configuration
 create_symlink "$DOTFILES_DIR/aerospace/.aerospace.toml" "$HOME/.aerospace.toml"
 
+# Zsh configuration (moved to .config/zsh)
+create_symlink "$DOTFILES_DIR/zsh/.zshrc" "$HOME/zsh/.zshrc"
+create_symlink "$DOTFILES_DIR/zsh/.p10k.zsh" "$HOME/.config/zsh/.p10k.zsh"
+
 # Add more configurations as needed
 # create_symlink "$DOTFILES_DIR/some_other_config" "$HOME/.some_other_config"
 
